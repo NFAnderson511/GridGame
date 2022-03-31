88eecd941bb4cbd30d3499af2bb539e5cf7045a4
Noah_Anderson <NoahFoxAnderson@gmail.com>
2022-03-31 18:25:52 -0400

    Fixed the placement of entered placement and asked for y coordinate before allowing input

--- src/App.java
+++ src/App.java
@@ -2,43 +2,47 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner keyboard = new Scanner(System.in);
-        int size = 20;
+        int xSize = 20;
+        int ySize = 20;
+        int yCenter = ySize/2;
+        int xCenter = xSize/2;
         System.out.print("Enter X Coordinate: ");
         int xPos = keyboard.nextInt();
-        while(xPos > size)
+        while(xPos > xSize)
         {
             System.out.print("Try Again: ");
             xPos = keyboard.nextInt();
         }
+            System.out.print("Enter Y Coordinate: ");
         int yPos = keyboard.nextInt();
-        while(yPos > size)
+        while(yPos > ySize)
         {
             System.out.print("Try Again: ");
             yPos = keyboard.nextInt();
         }
-        for(int y = 0; y <= size; y++)
+        for(int y = 0; y <= ySize; y++)
         {
-            for(int x = 0; x <= size; x++)
+            for(int x = 0; x <= xSize; x++)
             {
-                if(x == size/2 && y == size/2)
+                if(x == xCenter && y == yCenter)
                 {
-                    System.out.print("+");
+                    System.out.print("+ ");
                 }
-                else if(x == size/2)
+                else if(y == yCenter - yPos && x == xCenter + xPos)
                 {
-                    System.out.print("|");
+                    System.out.print("X ");
                 }
-                else if(y == size/2)
+                else if(x == xCenter)
                 {
-                    System.out.print("-");
+                    System.out.print("| ");
                 }
-                else if(y == yPos && x == xPos)
+                else if(y == yCenter)
                 {
-                    System.out.print("X");
+                    System.out.print("- ");
                 }
                 else
                 {
-                    System.out.print(".");
+                    System.out.print(". ");
                 }
             }
             System.out.println();
